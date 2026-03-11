01307a1c7d78fbd68567dcea23f6f87c389e03a3
Chris <115883974+Darkwolf7898@users.noreply.github.com>
2026-03-11 15:18:17 -0400

    max speed updated

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final double ROBOT_MASS = (45) * 0.453592; // 32lbs * kg per pound
 	public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
 	public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
-	public static final double MAX_SPEED = Units.feetToMeters(14.5);// 14.5);
+	public static final double MAX_SPEED = Units.feetToMeters(16.7);// 14.5);
 	public static final double kMaxSpeedScalar = 0.4;
 	public static final double kUnboostScalar = 0.1;
 	// Maximum speed of the robot in meters per second, used to limit acceleration.
